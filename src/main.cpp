4a452bd9ca230849b588718dd57393bc9c7193f8
Ilya_Kiselev <mybutifullie@gmail.com>
2023-11-20 12:51:59 +1000

    performed the necessary actions with curves

--- src/main.cpp
+++ src/main.cpp
@@ -1,14 +1,57 @@
 #include <iostream>
 #include <curvs.h>
+#include <algorithm>
+#include <numeric>
+
+void PrintCurvs(std::vector<std::shared_ptr<Curv>> curvs);
 
 int main()
 {
-    std::vector<std::unique_ptr<Curv>> curvs = Curv::GetVectorOfRandomCurvs();
+    Helixes3D helix(3.0f, 1.0f);
+    std::cout << helix.GetRadius() << std::endl;
+    std::vector<std::shared_ptr<Curv>> curvs = Curv::GetVectorOfRandomCurvs();
 
+    std::cout << "Curvs:\n"
+              << std::endl;
+
+    PrintCurvs(curvs);
+
+    std::vector<std::shared_ptr<Curv>> circles{};
+
+    std::copy_if(curvs.begin(), curvs.end(), std::back_inserter(circles),
+                 [](const std::shared_ptr<Curv> &ptr)
+                 {
+                     return ptr->GetType() == Curv::CIRCLE;
+                 });
+
+    std::cout << "Circles:\n"
+              << std::endl;
+
+    PrintCurvs(circles);
+
+    std::sort(circles.begin(), circles.end(), [](const std::shared_ptr<Curv> &ptrA, const std::shared_ptr<Curv> &ptrB)
+              { return ptrA->GetRadius() < ptrB->GetRadius(); });
+
+    std::cout << "Circles (sorted):\n"
+              << std::endl;
+
+    PrintCurvs(circles);
+
+    float sumRadii = std::accumulate(circles.begin(), circles.end(), 0.0f, [](float currentSum, const std::shared_ptr<Curv> &ptr)
+                                     { return currentSum + ptr->GetRadius(); });
+
+    std::cout << "sum of circle radii: " << sumRadii << std::endl;
+
+    return 0;
+}
+
+void PrintCurvs(std::vector<std::shared_ptr<Curv>> curvs)
+{
     for (size_t i = 0; i < curvs.size(); i++)
     {
         curvs.at(i)->print();
+        std::cout << "Point for PI: " << curvs.at(i)->GetPoint(PI) << std::endl;
+        std::cout << "First Derivative for PI: " << curvs.at(i)->GetFirstDerivative(PI) << "\n"
+                  << std::endl;
     }
-
-    return 0;
 }
